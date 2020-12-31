645941201687f4218407da7bf5c9e55ddd5bc426
sdvsoul <sdvsoulmate@gmail.com>
2020-12-31 10:48:45 +0530

    Update 2020/12/31 - 10:48am

--- marketing/models.py
+++ marketing/models.py
@@ -5,6 +5,8 @@ class Customer_Detail(models.Model):
     Customer_id = models.CharField(primary_key=True,max_length=60)
     Customer_name = models.CharField(max_length=100)
     Nick_name = models.CharField(max_length=30)
+    Contact = models.CharField(max_length = 50,default = None)
+    Email_id = models.EmailField(default = None)
     Address = models.TextField()
     GST_no = models.CharField(max_length=30)
     CIN_no = models.CharField(max_length=30)
